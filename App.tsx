58680a64490a401d73014a47f7fb067cf4915364
Viplav Patil <viplavdhande91@gmail.com>
2024-09-08 19:33:35 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App() {
+  return (
+    <View>
+      <Text style ={{margin: 'auto' , fontSize : 20}}>React Native TypeScript Project</Text>
+    </View>
+  );
+}
+
+
+export default App;
